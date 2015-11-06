fb9cf4613b14b1aa383437bec25b1926974d46e2
jonathanr4m <jonathanromeromiralles@outlook.com>
2015-11-06 12:36:05 +0000

    adssa

---
+++ sublime/Proyecto04/menu/formulario.php
@@ -0,0 +1,39 @@
+<html>
+    <body>
+       <div>
+           <center>
+       <form action="#" style="width: 250px; height: 200px;
+       
+       border-radius: 72px 72px 72px 72px;
+-moz-border-radius: 72px 72px 72px 72px;
+-webkit-border-radius: 72px 72px 72px 72px;
+border: 1px solid #a8a8a8;
+
+background: rgba(226,226,226,1);
+background: -moz-linear-gradient(left, rgba(226,226,226,1) 0%, rgba(219,219,219,1) 0%, rgba(209,209,209,1) 22%, rgba(254,254,254,1) 100%);
+background: -webkit-gradient(left top, right top, color-stop(0%, rgba(226,226,226,1)), color-stop(0%, rgba(219,219,219,1)), color-stop(22%, rgba(209,209,209,1)), color-stop(100%, rgba(254,254,254,1)));
+background: -webkit-linear-gradient(left, rgba(226,226,226,1) 0%, rgba(219,219,219,1) 0%, rgba(209,209,209,1) 22%, rgba(254,254,254,1) 100%);
+background: -o-linear-gradient(left, rgba(226,226,226,1) 0%, rgba(219,219,219,1) 0%, rgba(209,209,209,1) 22%, rgba(254,254,254,1) 100%);
+background: -ms-linear-gradient(left, rgba(226,226,226,1) 0%, rgba(219,219,219,1) 0%, rgba(209,209,209,1) 22%, rgba(254,254,254,1) 100%);
+background: linear-gradient(to right, rgba(226,226,226,1) 0%, rgba(219,219,219,1) 0%, rgba(209,209,209,1) 22%, rgba(254,254,254,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e2e2e2', endColorstr='#fefefe', GradientType=1 );" >
+           
+       
+        <br>
+        <b>Nombre:  </b><br><input type="text" name="nombreJugador"/>
+        <br>
+        <br>
+        <br>
+       <b>Edad: </b><br> <input type="text" name="edadJugador"/>
+        <br>
+        <br>
+        <input type="submit" class="btn btn-primary" name="submit" value="¡Jugar!"/>
+        </center>
+      </form>
+           </center>
+           </div>
+        
+       
+       
+    </body>
+</html>
